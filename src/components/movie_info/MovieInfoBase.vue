58aa8481fbaf967ba893f8d28e86b6547a1f88b9
root <root@kyojun-jin04.tn.corp.samsungelectronics.net>
2024-05-03 16:55:58 +0900

    KAN-21 / 영화 정보 통합 후 하단 탭 만들기

--- src/components/movie_info/MovieInfoBase.vue
+++ src/components/movie_info/MovieInfoBase.vue
@@ -1,14 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineEmits<{ (e: "changeCategory", category: string): void }>();
+
+const movieInfo = [
+  { name: "영화의 역사", category: "history" },
+  { name: "다양한 장르", category: "genre" },
+  { name: "영화 방법론", category: "methodology" },
+  { name: "관련 사이트", category: "website" },
+  { name: "단어 사전", category: "dictionary" },
+];
+</script>
 
 <template>
-  <v-row>
-    <v-col cols="2">
-      <slot name="sidebar"></slot>
-    </v-col>
-    <v-col>
-      <slot name="section"></slot>
-    </v-col>
-  </v-row>
+  <v-card>
+    <v-tabs align-tabs="title">
+      <v-tab
+        v-for="(m, i) in movieInfo"
+        :key="i"
+        @click="$emit('changeCategory', m.category)"
+      >
+        {{ m.name }}
+      </v-tab>
+    </v-tabs>
+
+    <v-row>
+      <v-col cols="2">
+        <slot name="sidebar"></slot>
+      </v-col>
+      <v-col>
+        <slot name="section"></slot>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 
 <style scoped></style>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,11 +1,13 @@
 import MovieDefinition from "../components/MovieDefinition.vue";
 import MovieHome from "../components/MovieHome.vue";
-import MovieInfoBase from "../components/movie_info/MovieInfo.vue";
+import MovieInfo from "../components/movie_info/MovieInfo.vue";
+import MovieForum from "../components/movie_forum/MovieForum.vue";
 
 const routes = [
   { path: "/", component: MovieHome },
   { path: "/what-is-movie", component: MovieDefinition },
-  { path: "/info/:category", component: MovieInfoBase, props: true },
+  { path: "/info", component: MovieInfo },
+  { path: "/forum", component: MovieForum },
 ];
 
 export { routes };
